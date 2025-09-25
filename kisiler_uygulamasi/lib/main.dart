42835bd622ed7ef1d738d095df98a9480317d8e2
Ebrardemir <ebrardemir6106@gmail.com>
2025-09-25 13:02:16 +0300

    uı tasarımı, silme-ekleme ve listeleme işlemleri yapıldı

--- kisiler_uygulamasi/lib/main.dart
+++ kisiler_uygulamasi/lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:kisiler_uygulamasi/models/contact.dart';
+import 'package:kisiler_uygulamasi/pages/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); //async işlemler olduğu için kullanılıyo
-  
+
   await Hive.initFlutter(); //hive i başlatma
 
   Hive.registerAdapter(ContactAdapter()); //adapteri kaydetmek için
-  await Hive.openBox<Contact>('contacts'); //contacts adında box açtık , contact tipinde
+  await Hive.openBox<Contact>(
+    'contacts',
+  ); //contacts adında box açtık , contact tipinde
 
   runApp(const MyApp());
 }
@@ -18,6 +22,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Kişilerim",
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 202, 205, 248),
+        scaffoldBackgroundColor: Colors.grey[100],
+
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.deepPurple,
+          elevation: 4,
+          centerTitle: true,
+
+          titleTextStyle: GoogleFonts.poppins(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+      ),
+      home: HomeScreen(),
+    );
   }
 }
